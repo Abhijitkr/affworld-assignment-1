3a253c17be9d9a3aa55ada63ec322a7985b1b118
AbhijitKr <abhijitkr600@gmail.com>
2024-01-14 16:39:36 +0530

    Implements apis

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const secretRouter = require("./routes/secret-route");
 
 require("./db");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/secrets", secretRouter);
 
 app.use("/api", (req, res) => {
   res.send("Hello World");
