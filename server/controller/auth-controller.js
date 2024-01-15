c5297beba949129e218305f4efa6fa61f79a1fa6
AbhijitKr <abhijitkr600@gmail.com>
2024-01-15 20:26:52 +0530

    Hashes the password and stores using bcrypt

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,4 +1,5 @@
 const User = require("../model/User");
+const bcrypt = require("bcrypt");
 
 // Registration Logic
 const register = async (req, res) => {
@@ -11,15 +12,19 @@ const register = async (req, res) => {
     return res.status(400).json({ msg: "User already exists" });
   }
 
+  //hashing password using bcrypt
+  const saltRound = 10;
+  const hash_password = await bcrypt.hash(password, saltRound);
+
   const newUser = await User.create({
     username,
     email,
-    password,
+    password: hash_password,
     createdDate: currentDate,
   });
 
   try {
-    console.log(req.body);
+    console.log(newUser);
     res.status(200).json({ newUser });
   } catch (error) {
     res.status(500).json({ msg: "Internal server error" });
