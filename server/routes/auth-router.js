b385c2dabf0190edd66d8e287d78f3e76760d764
AbhijitKr <abhijitkr600@gmail.com>
2024-01-15 23:18:40 +0530

    Adds validation using zod and validates as middleware to registration

--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -2,8 +2,10 @@ const express = require("express");
 const authRouter = express();
 
 const { register, login } = require("../controller/auth-controller");
+const signupSchema = require("../validator/auth-validator");
+const validate = require("../middleware/validate-middleware");
 
-authRouter.post("/register", register);
+authRouter.post("/register", validate(signupSchema), register);
 authRouter.post("/login", login);
 
 module.exports = authRouter;
