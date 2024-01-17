625598c9f36b83f417a50ea644610297f2a4c0b7
AbhijitKr <abhijitkr600@gmail.com>
2024-01-17 18:34:52 +0530

    Adds front end build and configure backend to run through server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const authRouter = require("./routes/auth-router");
 const secretRouter = require("./routes/secret-route");
 const errorMiddleware = require("./middleware/error-middleware");
+const path = require("path");
 
 require("./db");
 
@@ -11,12 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.resolve(__dirname, "dist")));
 app.use("/api/auth", authRouter);
 app.use("/api/secrets", secretRouter);
 
 // app.use("/api", (req, res) => {
 //   res.send("Hello World");
 // });
+app.get("*", (req, res) => res.sendFile(path.resolve("dist", "index.html")));
 
 app.use(errorMiddleware);
 
